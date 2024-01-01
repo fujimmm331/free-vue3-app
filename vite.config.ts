1306b0448059d96ac7ffd46126644be378d14d5f
fujimmm331 <nyappy15th@gmail.com>
2024-01-01 21:03:59 +0900

    feat: aliasの設定

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,16 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@config': fileURLToPath(new URL('./src/config', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@services': fileURLToPath(new URL('./src/services', import.meta.url)),
+      '@models': fileURLToPath(new URL('./src/models', import.meta.url)),
+      '@const': fileURLToPath(new URL('./src/const', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@containers': fileURLToPath(new URL('./src/containers', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
     }
   }
 })
